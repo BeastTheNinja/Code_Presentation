dbc823a7aa9eb7c49d9e4f9c31512bf04ca8ce1e
BeastTheNinja <134609528+BeastTheNinja@users.noreply.github.com>
2025-03-14 12:59:34 +0100

    done

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,24 @@
-console.log(HELLLO);
+// >= betyder less than or equal
+// <= betyder greater than or equal too
+
+let A = 10
+
+let B = 20
+
+if (A >= B) {
+    console.log(true);
+}
+else{
+    console.log(false);   
+}
+
+if(A <= B){
+    console.log(true);
+    
+}
+else{
+    console.log(false);
+    
+}
+
+// man kan bruge disse if else statements i javascript hvis man vil begrænse adgang fra en bevist aldersgruppe, man bruger det for eksempel på facebook når man skal tilmelde sig når man skal være 13+, og man kan gøre det hvs man skal leje en film, der kan man bruge aldersgrænsen ude fra P rating, some pg-13 pg T M A
